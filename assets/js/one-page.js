38d08ec05278f405afdd12cda446b0170e6d608a
Jiri Cermak <jiri.cermak482@gmail.com>
2014-12-05 00:32:51 +0100

    Updated "load more" function

--- assets/js/one-page.js
+++ assets/js/one-page.js
@@ -7,308 +7,333 @@
  * */
 
 $(document).ready(function () {
-    
-    /*
-     * ===================================
-     * Back To Top
-     * ===================================
-     * */
 
-    var offset = 220,
-        duration = 500;
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > offset) {
-            $('.back-to-top').fadeIn(duration);
-        } else {
-            $('.back-to-top').fadeOut(duration);
-        }
-    });
+	/*
+	 * ===================================
+	 * Back To Top
+	 * ===================================
+	 * */
 
-    $('.back-to-top').click(function (event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, duration);
-        return false;
-    });
+	var offset = 220,
+		duration = 500;
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > offset) {
+			$('.back-to-top').fadeIn(duration);
+		} else {
+			$('.back-to-top').fadeOut(duration);
+		}
+	});
 
-
-    /*
-     * ===================================
-     * Smooth scroll - from href to id
-     * ===================================
-     * */
-
-    // Must add the class "scroll" to the link - <a href="#someID" class="scroll">
-    $('a.scroll').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 750);
-                return false;
-            }
-        }
-    });
+	$('.back-to-top').click(function (event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: 0
+		}, duration);
+		return false;
+	});
 
 
-    //=== navigation settings ===//
+	/*
+	 * ===================================
+	 * Smooth scroll - from href to id
+	 * ===================================
+	 * */
 
-    var navbar = $("#navbarSettings");
-
-
-    /*=== Transparent navigation ===*/
-
-    // add background color to transparent navbar after scrolling 90px
-    $(window).scroll(function () {
-        var $navbarTrn = $(".navbar-trn"),
-            $nav = $(".navbar");
-        if (navbar.hasClass("navbar-fixed-top")) {
-            if ($(window).scrollTop() > 0) {
-                $navbarTrn.css({
-                    "background-color": "rgba(0, 0, 0, 0.8)"
-                });
-                $nav.css({
-                    "box-shadow": "0 2px 5px rgba(0, 0, 0, 0.2)"
-                });
-                $nav.removeClass("navbar-height");
-            } else {
-                $nav.css({
-                    "box-shadow": "none"
-                });
-                $navbarTrn.css({
-                    "background-color": "rgba(255,255,255,0.5);"
-                });
-                $nav.addClass("navbar-height");
-            }
-        } else {
-            $navbarTrn.css({
-                "background-color": "rgba(255,255,255,0.5);"
-            });
-            $nav.css({
-                "box-shadow": "none"
-            });
-            $nav.addClass("navbar-height");
-        }
-    });
+	// Must add the class "scroll" to the link - <a href="#someID" class="scroll">
+	$('a.scroll').click(function () {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 750);
+				return false;
+			}
+		}
+	});
 
 
-    //Navigation for mobile devices - when you click a link in navigation, navigation will collapse
-    $(".nav").find(".dropdown").on("click", function () {
-        $(".navbar-collapse").removeClass("in");
-    });
+	//=== navigation settings ===//
 
-    //When you open the menu on mobile devices, the icon will be changed
-    var $navToggle = $(".navbar-toggle");
-
-    if ($navToggle.hasClass("navbar-open")) {
-        ("navbar-open").on("click", function () {
-            $(this).removeClass("navbar-open");
-        });
-    } else {
-        $navToggle.on("click", function () {
-            $navToggle.toggleClass("navbar-open");
-        });
-    }
-
-    $(".navbar-nav").find(".dropdown").on("click", function () {
-        $navToggle.removeClass("navbar-open");
-    });
-
-    /*
-     * ===================================
-     * Load next news
-     * ===================================
-     * */
-
-    $("#loadNews").on("click", function () {
-        $("#newsFeed").find(".box-feed").last().load("assets/ajax-load/news.html");
-    });
-    $(document).ajaxComplete(function (event, request, settings) {
-        $(".box-load").animate({
-            "opacity": "1"
-        });
-        $(".box-load").removeClass("box-load");
-    });
-
-    /* 
-     * ===================================
-     * Animations during scrolling down using jQuery WayPoints plugin
-     * ===================================
-     * */
-
-    //our city section
-    $("#sOurCity").waypoint(function () {
-        setTimeout(function () {
-            $("#pricesSlider").find(".first").removeClass("flipping");
-        }, 850);
-
-        setTimeout(function () {
-            $("#pricesSlider").find(".second").removeClass("flipping");
-        }, 1050);
-
-        setTimeout(function () {
-            $("#pricesSlider").find(".third").removeClass("flipping");
-        }, 1250);
-
-        setTimeout(function () {
-            $("#pricesSlider").find(".fourth").removeClass("flipping");
-        }, 1450);
-    });
+	var navbar = $("#navbarSettings");
 
 
-    //news section
-    $("#sNews").waypoint(function () {
-        $("#newsFeed").addClass("timelineEffect");
-        setTimeout(function () {
-            $(".box-post:nth-child(1)").removeClass("fadeInUp");
-        }, 450);
-    });
+	/*=== Transparent navigation ===*/
 
-    //legacy section
-    $("#sLegacy").waypoint(function () {
-        $("#la").addClass("slideInEffect");
-    });
-    $("#la").waypoint(function () {
-        $("#lb").addClass("slideInEffect");
-    });
-    $("#lb").waypoint(function () {
-        $("#lc").addClass("slideInEffect");
-    });
-
-
-    //--------------------------------------------
-
-
-    /*
-     * ===================================
-     * Settings for plugins
-     * ===================================
-     * */
-
-
-    /*
-     * ===================================
-     * Smooth scrolling
-     * ===================================
-     * */
-
-    $("html").niceScroll({
-        cursorcolor: "rgba(0, 0, 0, 0.45)",
-        cursorwidth: "15px",
-        cursorborder: "none",
-        cursorborderradius: "0px",
-        scrollspeed: 60,
-        mousescrollstep: 45,
-        hwacceleration: true,
-        background: "rgba(0, 0, 0, 0.15)",
-        preservenativescrolling: true,
-        bouncescroll: false,
-        spacebarenabled: true,
-        disableoutline: true,
-        enablemousewheel: true, // nicescroll can manage mouse wheel events
-        enablekeyboard: true,
-        smoothscroll: true,
-        sensitiverail: true,
-        autohidemode: false,
-        gesturezoom: true, // (only when boxzoom=true and with touch devices) zoom activated when pinch out/in on box
-        grabcursorenabled: true, // (only when touchbehavior=true) display "grab" icon
-        enablescrollonselection: true,
-        cursordragspeed: 0.3,
-        zindex: 999999,
-    });
-
-    /*
-     * ===================================
-     * Modal Effects
-     * ===================================
-     * */
-    $('.md-trigger').modalEffects({
-        overlaySelector: '.md-overlay',
-        closeSelector: '.md-close',
-        classAddAfterOpen: 'md-show',
-        modalAttr: 'data-modal',
-        perspectiveClass: 'md-perspective',
-        perspectiveSetClass: 'md-setperspective',
-        afterOpen: function (button, modal) {
-
-        },
-        afterClose: function (button, modal) {
-
-        }
-    });
+	// add background color to transparent navbar after scrolling 90px
+	$(window).scroll(function () {
+		var $navbarTrn = $(".navbar-trn"),
+			$nav = $(".navbar");
+		if (navbar.hasClass("navbar-fixed-top")) {
+			if ($(window).scrollTop() > 0) {
+				$navbarTrn.css({
+					"background-color": "rgba(0, 0, 0, 0.8)"
+				});
+				$nav.css({
+					"box-shadow": "0 2px 5px rgba(0, 0, 0, 0.2)"
+				});
+				$nav.removeClass("navbar-height");
+			} else {
+				$nav.css({
+					"box-shadow": "none"
+				});
+				$navbarTrn.css({
+					"background-color": "rgba(255,255,255,0.5);"
+				});
+				$nav.addClass("navbar-height");
+			}
+		} else {
+			$navbarTrn.css({
+				"background-color": "rgba(255,255,255,0.5);"
+			});
+			$nav.css({
+				"box-shadow": "none"
+			});
+			$nav.addClass("navbar-height");
+		}
+	});
 
 
-    /*
-     * ===================================
-     * Revolution slider
-     * ===================================
-     * */
-    jQuery('.tp-banner').show().revolution({
-        dottedOverlay: "none",
-        delay: 16000,
-        startwidth: 1170,
-        startheight: 700,
-        hideThumbs: 200,
+	//Navigation for mobile devices - when you click a link in navigation, navigation will collapse
+	$(".nav").find(".dropdown").on("click", function () {
+		$(".navbar-collapse").removeClass("in");
+	});
 
-        thumbWidth: 100,
-        thumbHeight: 50,
-        thumbAmount: 5,
+	//When you open the menu on mobile devices, the icon will be changed
+	var $navToggle = $(".navbar-toggle");
 
-        navigationType: "none",
-        navigationArrows: "none",
-        navigationStyle: "none",
+	if ($navToggle.hasClass("navbar-open")) {
+		("navbar-open").on("click", function () {
+			$(this).removeClass("navbar-open");
+		});
+	} else {
+		$navToggle.on("click", function () {
+			$navToggle.toggleClass("navbar-open");
+		});
+	}
 
-        touchenabled: "off",
-        onHoverStop: "off",
+	$(".navbar-nav").find(".dropdown").on("click", function () {
+		$navToggle.removeClass("navbar-open");
+	});
 
-        swipe_velocity: 0.7,
-        swipe_min_touches: 1,
-        swipe_max_touches: 1,
-        drag_block_vertical: false,
+	/*
+	 * ===================================
+	 * Load next news
+	 * ===================================
+	 * */
+	//	$.ajax({
+	//		type: "GET",
+	//		url: "assets/ajax-load/news.html",
+	//		async: false,
+	//		success: function (text) {
+	//			ajaxContent = text;
+	//		}
+	//	});
 
-        parallax: "mouse",
-        parallaxBgFreeze: "on",
-        parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+	var ajaxContent1,
+		ajaxContent2,
+		ajaxContent3,
+		ajaxContent4,
+		ajaxContent5;
+	$.get("assets/ajax-load/news1.html", function (data) {
+		ajaxContent1 = data;
+	});
+	$.get("assets/ajax-load/news2.html", function (data) {
+		ajaxContent2 = data;
+	});
+	$.get("assets/ajax-load/news3.html", function (data) {
+		ajaxContent3 = data;
+	});
+	$.get("assets/ajax-load/news4.html", function (data) {
+		ajaxContent4 = data;
+	});
+	$.get("assets/ajax-load/news5.html", function (data) {
+		ajaxContent5 = data;
+	});
 
-        shadow: 0,
-        fullWidth: "on",
-        fullScreen: "off",
-        spinner: "spinner4",
-        autoHeight: "off",
-        forceFullWidth: "on",
-        hideTimerBar: "on"
-    });
+	$("#loadNews").on("click", function () {
+		$("#loadHere").append("<div class='row box-feed'>" + ajaxContent1 + "</div>" + "<div class='row box-feed'>" + ajaxContent2 + "</div>" + "<div class='row box-feed'>" + ajaxContent3 + "</div>" + "<div class='row box-feed'>" + ajaxContent4 + "</div>" + "<div class='row box-feed'>" + ajaxContent5 + "</div>");
+		$("#loadNews").hide();
+		$("#messageAfter").show();
+	});
+
+	/* 
+	 * ===================================
+	 * Animations during scrolling down using jQuery WayPoints plugin
+	 * ===================================
+	 * */
+
+	//our city section
+	$("#sOurCity").waypoint(function () {
+		setTimeout(function () {
+			$("#pricesSlider").find(".first").removeClass("flipping");
+		}, 850);
+
+		setTimeout(function () {
+			$("#pricesSlider").find(".second").removeClass("flipping");
+		}, 1050);
+
+		setTimeout(function () {
+			$("#pricesSlider").find(".third").removeClass("flipping");
+		}, 1250);
+
+		setTimeout(function () {
+			$("#pricesSlider").find(".fourth").removeClass("flipping");
+		}, 1450);
+	});
 
 
-    /*
-     * ===================================
-     * Owl Carousel
-     * ===================================
-     * */
+	//news section
+	$("#sNews").waypoint(function () {
+		$("#newsFeed").addClass("timelineEffect");
+		setTimeout(function () {
+			$(".box-post:nth-child(1)").removeClass("fadeInUp");
+		}, 450);
+	});
 
-    //Numbers slider
-    $("#pricesSlider").owlCarousel({
-        autoPlay: false, //Set AutoPlay to 3 seconds
-        items: 4,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3]
-    });
-
-    //Lastest tweets slider
-    $("#twitterSlider").owlCarousel({
-        autoPlay: true, //Set AutoPlay to 3 seconds
-        items: 1,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3]
-    });
+	//legacy section
+	$("#sLegacy").waypoint(function () {
+		$("#la").addClass("slideInEffect");
+	});
+	$("#la").waypoint(function () {
+		$("#lb").addClass("slideInEffect");
+	});
+	$("#lb").waypoint(function () {
+		$("#lc").addClass("slideInEffect");
+	});
 
 
-    /*=== Validator for form ===*/
-    $(".validate").validate();
+	//--------------------------------------------
 
-    /*=== Placeholders ===*/
-    $('input, textarea').placeholder();
+
+	/*
+	 * ===================================
+	 * Settings for plugins
+	 * ===================================
+	 * */
+
+
+	/*
+	 * ===================================
+	 * Smooth scrolling
+	 * ===================================
+	 * */
+
+	$("html").niceScroll({
+		cursorcolor: "rgba(0, 0, 0, 0.45)",
+		cursorwidth: "15px",
+		cursorborder: "none",
+		cursorborderradius: "0px",
+		scrollspeed: 60,
+		mousescrollstep: 45,
+		hwacceleration: true,
+		background: "rgba(0, 0, 0, 0.15)",
+		preservenativescrolling: true,
+		bouncescroll: false,
+		spacebarenabled: true,
+		disableoutline: true,
+		enablemousewheel: true, // nicescroll can manage mouse wheel events
+		enablekeyboard: true,
+		smoothscroll: true,
+		sensitiverail: true,
+		autohidemode: false,
+		gesturezoom: true, // (only when boxzoom=true and with touch devices) zoom activated when pinch out/in on box
+		grabcursorenabled: true, // (only when touchbehavior=true) display "grab" icon
+		enablescrollonselection: true,
+		cursordragspeed: 0.3,
+		zindex: 999999
+	});
+
+	/*
+	 * ===================================
+	 * Modal Effects
+	 * ===================================
+	 * */
+	$('.md-trigger').modalEffects({
+		overlaySelector: '.md-overlay',
+		closeSelector: '.md-close',
+		classAddAfterOpen: 'md-show',
+		modalAttr: 'data-modal',
+		perspectiveClass: 'md-perspective',
+		perspectiveSetClass: 'md-setperspective',
+		afterOpen: function (button, modal) {
+
+		},
+		afterClose: function (button, modal) {
+
+		}
+	});
+
+
+	/*
+	 * ===================================
+	 * Revolution slider
+	 * ===================================
+	 * */
+	jQuery('.tp-banner').show().revolution({
+		dottedOverlay: "none",
+		delay: 16000,
+		startwidth: 1170,
+		startheight: 700,
+		hideThumbs: 200,
+
+		thumbWidth: 100,
+		thumbHeight: 50,
+		thumbAmount: 5,
+
+		navigationType: "none",
+		navigationArrows: "none",
+		navigationStyle: "none",
+
+		touchenabled: "off",
+		onHoverStop: "off",
+
+		swipe_velocity: 0.7,
+		swipe_min_touches: 1,
+		swipe_max_touches: 1,
+		drag_block_vertical: false,
+
+		parallax: "mouse",
+		parallaxBgFreeze: "on",
+		parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+
+		shadow: 0,
+		fullWidth: "on",
+		fullScreen: "off",
+		spinner: "spinner4",
+		autoHeight: "off",
+		forceFullWidth: "on",
+		hideTimerBar: "on"
+	});
+
+
+	/*
+	 * ===================================
+	 * Owl Carousel
+	 * ===================================
+	 * */
+
+	//Numbers slider
+	$("#pricesSlider").owlCarousel({
+		autoPlay: false, //Set AutoPlay to 3 seconds
+		items: 4,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [979, 3]
+	});
+
+	//Lastest tweets slider
+	$("#twitterSlider").owlCarousel({
+		autoPlay: true, //Set AutoPlay to 3 seconds
+		items: 1,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [979, 3]
+	});
+
+
+	/*=== Validator for form ===*/
+	$(".validate").validate();
+
+	/*=== Placeholders ===*/
+	$('input, textarea').placeholder();
 });
